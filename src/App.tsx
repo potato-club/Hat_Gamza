e60ead426670939e80ed3b92ad31c4faa98e5c3b
JaiIn <di0308@naver.com>
2024-07-16 00:02:24 +0900

    style: 중앙배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './global.css';
 import Header from './components/header/Header';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <>
-      <Header/>
-    </>
+    <Container>
+        <Header/>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
