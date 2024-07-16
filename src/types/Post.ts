5673ae43ee3871c2e87e755955538d822cdc3066
JaiIn <di0308@naver.com>
2024-07-16 22:16:07 +0900

    feat: post,user 선언 수정

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,3 +1,8 @@
 export interface Post{
-    id:number
+    id:number;
+    title:string;
+    content:string;
+    createdAt:string;
+    updatedAt:string;
+    UserId:number;
 }
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,4 +1,14 @@
 export interface User{
     id:number;
-    name:string
+    name:string;
+    username:string;
+    email:string;
+    website:string;
+    province:string;
+    city:string;
+    district:string;
+    street:string;
+    zipcode:string
+    createdAt:string;
+    updatedAt:string
 }
