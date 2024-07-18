ea1b612473c8c31565efba7c85d48ed19caa534f
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-07-18 21:50:56 +0900

    Refactor: usePaginate hook 수정

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -4,14 +4,18 @@ import { fetchPost } from "../../api/posts";
 import { fetchUser } from "../../api/userInfo";
 import PostItem from "./PostItem";
 import styled from "styled-components";
+import usePagination from "../../hooks/usePaginate";
 
-interface PostUser extends Post {
+export interface PostUser extends Post {
   name: string;
 }
 
 function PostList() {
   const [posts, setPosts] = useState<PostUser[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const postsPerPage = 4;
+  const { totalPages, jumpToPage, currentData, viewPageNumber, currentPage } =
+    usePagination(posts, postsPerPage);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,9 +27,10 @@ function PostList() {
           const user = usersData.find((user) => user.id === post.UserId);
           return { ...post, name: user?.name || "Unknown" };
         });
-        setPosts(postsWithUser.slice(0, 4));
+
+        setPosts(postsWithUser);
       } catch (error) {
-        setError("errror !");
+        setError("Error fetching data!");
         console.error(error);
       }
     };
@@ -36,13 +41,29 @@ function PostList() {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <PostListContainer>
-      <PostBox>
-        {posts.map((post) => (
-          <PostItem key={post.id} post={post} />
+    <div>
+      <PostListContainer>
+        <PostBox>
+          {currentData.map((post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </PostBox>
+      </PostListContainer>
+
+      <Pagination>
+        {currentPage > 1 && (
+          <button onClick={() => jumpToPage(currentPage - 1)}> {"<"} </button>
+        )}
+        {viewPageNumber.map((page, index) => (
+          <button key={index} onClick={() => jumpToPage(page)}>
+            {page}
+          </button>
         ))}
-      </PostBox>
-    </PostListContainer>
+        {currentPage < totalPages && (
+          <button onClick={() => jumpToPage(currentPage + 1)}> {">"} </button>
+        )}
+      </Pagination>
+    </div>
   );
 }
 
@@ -51,16 +72,26 @@ const PostListContainer = styled.div`
   width: 1243px;
   height: 491px;
   background-color: aliceblue;
+  justify-content: center;
+  flex-direction: column;
   align-items: center;
 `;
 
 const PostBox = styled.div`
   display: flex;
-  width: 100%;
-  height: 60%;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+`;
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  button {
+    margin: 0 0.5rem;
+    padding: 0.5rem 1rem;
+    border: none;
+    cursor: pointer;
+  }
 `;
 
 export default PostList;
